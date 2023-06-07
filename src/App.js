7c305616085d248183ac4d24bfc40cd54b52d05a
Naseba <naseba.hamee@gmail.com>
2023-06-07 18:36:19 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import "./quran2.jpg";
+import {LanguagesAvailable} from "./LanguageAvailable";
+import {useState,useEffect} from 'react';
+import {API} from "./LanguageAvailable";
+
+
 
 function App() {
+
+  const [selectedItem,setSelectedItem]=useState("");
+  const [bookData,setBookData]=useState([]);
+
+  useEffect(()=>{
+  fetch(`${API}`)
+  .then((res)=>res.json())
+  .then((data)=>{
+    setBookData(data);
+      });
+  
+},[]);
+
+  const handleChange=(event)=>{
+    setSelectedItem(event.target.value);
+  }
+  let filteredItem=[];
+  if(bookData.length>0){
+ filteredItem = bookData.filter((item) => item.language === selectedItem);
+  console.log(filteredItem);
+}
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <select value={selectedItem} onChange={handleChange} className="languages">
+        {LanguagesAvailable.map((item, id) => (
+          <option key={id} value={item.name}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+      <div className='section'>
+        <div className='langdiv'><p>Language :      {selectedItem}</p></div>{
+        filteredItem.map((item)=>(
+          <div key={item.id}>{item.link}</div>
+        ))}
+      
+      </div>
+      
     </div>
   );
 }
---
+++ src/LanguageAvailable.js
@@ -0,0 +1,297 @@
+export const API="https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.json"
+export const LanguagesAvailable = [
+  {
+    name: "Achinese",
+  },
+  {
+    name: "Afar",
+  },
+  {
+    name: "Afrikaans",
+  },
+  {
+    name: "Albanian",
+  },
+  {
+    name: "Amharic",
+  },
+  {
+    name: "Arabic",
+  },
+  {
+    name: "Assamese",
+  },
+  {
+    name: "Azerbaijani",
+  },
+  {
+    name: "Bambara",
+  },
+  {
+    name: "Bengali",
+  },
+  {
+    name: "Berber",
+  },
+  {
+    name: "Bosnian",
+  },
+  {
+    name: "Bulgarian",
+  },
+  {
+    name: "Burmese",
+  },
+  {
+    name: "Catalan",
+  },
+  {
+    name: "Chichewa",
+  },
+  {
+    name: "Chinese-simplified",
+  },
+  {
+    name: "Chinese-Traditional",
+  },
+  {
+    name: "Croatian",
+  },
+  {
+    name: "Czech",
+  },
+  {
+    name: "Dagbani",
+  },
+  {
+    name: "Dari",
+  },
+  {
+    name: "Danish",
+  },
+  {
+    name: "Divehi",
+  },
+  {
+    name: "Dutch",
+  },
+  {
+    name: "English",
+  },
+  {
+    name: "Esperanto",
+  },
+  {
+    name: "Filipino",
+  },
+  {
+    name: "Finnish",
+  },
+  {
+    name: "French",
+  },
+  {
+    name: "Fulah",
+  },
+  {
+    name: "Ganda",
+  },
+  {
+    name: "German",
+  },
+  {
+    name: "Gujarati",
+  },
+  {
+    name: "Hausa",
+  },
+  {
+    name: "Hebrew",
+  },
+  {
+    name: "Hindi",
+  },
+  {
+    name: "Hungarian",
+  },
+  {
+    name: "Indonesian",
+  },
+  {
+    name: "Iranun",
+  },
+  {
+    name: "Italian",
+  },
+  {
+    name: "Japanese",
+  },
+  {
+    name: "Javanese",
+  },
+  {
+    name: "Kannada",
+  },
+  {
+    name: "Kazakh",
+  },
+  {
+    name: "Kendayan",
+  },
+  {
+    name: "Khmer",
+  },
+  {
+    name: "Kinyarwanda",
+  },
+  {
+    name: "Kirghiz",
+  },
+  {
+    name: "Korean",
+  },
+  {
+    name: "Kurdish",
+  },
+  {
+    name: "Kurmanji",
+  },
+  {
+    name: "Latin",
+  },
+  {
+    name: "Lingala",
+  },
+  {
+    name: "Luyia",
+  },
+  {
+    name: "Macedonian",
+  },
+  {
+    name: "Malay",
+  },
+  {
+    name: "Malayalam",
+  },
+  {
+    name: "Maltese",
+  },
+  {
+    name: "Maranao",
+  },
+  {
+    name: "Marathi",
+  },
+  {
+    name: "Nepali",
+  },
+  {
+    name: "Norwegian",
+  },
+  {
+    name: "Oromo",
+  },
+  {
+    name: "Panjabi",
+  },
+  {
+    name: "Persian",
+  },
+  {
+    name: "Polish",
+  },
+  {
+    name: "Portuguese",
+  },
+  {
+    name: "Pushto",
+  },
+  {
+    name: "Romanian",
+  },
+  {
+    name: "Rundi",
+  },
+  {
+    name: "Russian",
+  },
+  {
+    name: "Serbian",
+  },
+  {
+    name: "Shona",
+  },
+  {
+    name: "Sindhi",
+  },
+  {
+    name: "Sinhala",
+  },
+  {
+    name: "Slovak",
+  },
+  {
+    name: "Somali",
+  },
+  {
+    name: "Sotho",
+  },
+  {
+    name: "Spanish",
+  },
+  {
+    name: "Swahili",
+  },
+  {
+    name: "Swedish",
+  },
+  {
+    name: "Tajik",
+  },
+  {
+    name: "Tamil",
+  },
+  {
+    name: "Tatar",
+  },
+  {
+    name: "Telugu",
+  },
+  {
+    name: "Thai",
+  },
+  {
+    name: "Turkish",
+  },
+  {
+    name: "Twi",
+  },
+  {
+    name: "Uighur",
+  },
+  {
+    name: "Ukrainian",
+  },
+  {
+    name: "Urdu",
+  },
+  {
+    name: "Uzbek",
+  },
+  {
+    name: "Vietnamese",
+  },
+  {
+    name: "Xhosa",
+  },
+  {
+    name: "Yau",
+  },
+  {
+    name: "Yoruba",
+  },
+  {
+    name: "Zulu",
+  },
+];
