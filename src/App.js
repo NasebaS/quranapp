cfc8eac9ca4d344503ec8105dd7afdf413f814b8
Naseba <naseba.hamee@gmail.com>
2023-06-13 18:39:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import "./quran2.jpg";
-import {LanguagesAvailable} from "./LanguageAvailable";
-import React,{useState,useEffect} from 'react';
-import {API} from "./LanguageAvailable";
-
+import { LanguagesAvailable } from "./LanguageAvailable";
+import React, { useState, useEffect } from 'react';
+import { API } from "./LanguageAvailable";
 
 function ErrorBoundary({ children }) {
   const [hasError, setHasError] = useState(false);
@@ -26,63 +24,69 @@ function ErrorBoundary({ children }) {
 }
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState("");
+  const [bookData, setBookData] = useState([]);
+  const [boxDataArray, setBoxDataArray] = useState([]);
+  const [selectedChapter, setSelectedChapter] = useState("");
+  const [chapterDataArray, setChapterDataArray] = useState([]);
 
-  const [selectedItem,setSelectedItem]=useState("");
-  const [bookData,setBookData]=useState([]);
-  const [boxDataArray,setBoxDataArray]=useState([]);
-  const [selectedChapter,setSelectedChapter] =useState("");
-  const [chapterDataArray,setChapterDataArray]=useState([]);
-
-
-
-  useEffect(()=>{
-  fetch(`${API}`)
-  .then((res)=>res.json())
-  .then((data)=>{
-    const api=Object.values(data);
-    setBookData(api);
+  useEffect(() => {
+    fetch(`${API}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const api = Object.values(data);
+        setBookData(api);
+      })
+      .catch((error) => {
+        console.error("Error fetching book data:", error);
       });
-},[]);
+  }, []);
 
-  const handleChange=(event)=>{
-    setSelectedItem(event.target.value);
-  }
-  const handleChapterChange=(event)=>{
-    setSelectedChapter(event.target.value);
+  useEffect(() => {
+    if (selectedItem) {
+      let filteredItem = bookData.find((item) => item.language === selectedItem);
+      if (filteredItem) {
+        fetch(filteredItem.link)
+          .then((res) => res.json())
+          .then((data) => {
+            setBoxDataArray(data); 
+                   
+          })
+          .catch((error) => {
+            console.error("Error fetching box data:", error);
+          });
+      }
     }
-  useEffect(()=>{
-    if(selectedItem){
-      let filteredItem=[];
-filteredItem = bookData.find((item) => item.language === selectedItem);
-if(filteredItem){
-  fetch(filteredItem.link)
-.then((res)=>res.json())
-.then((data)=>{  
-setBoxDataArray(data.quran)
-})}    }
-  },[bookData,selectedItem])
+  }, [bookData, selectedItem]);
 
-  useEffect(()=>{
-    if(selectedChapter){
-      let filteredChapter=[];
-      filteredChapter=boxDataArray.quran.find((item)=>item.chapter===selectedChapter);
-      if(filteredChapter){
+   useEffect(() => {
+    if (selectedChapter && boxDataArray.length > 0) {
+      let filteredChapter=[]
+     filteredChapter = boxDataArray.quran.find((item) => item.chapter === selectedChapter);
+       console.log(filteredChapter)
+      if (filteredChapter) {
+        
         fetch(filteredChapter)
         .then((res)=>res.json())
         .then((data)=>{
-          setChapterDataArray(data)
-          console.log(filteredChapter)
-        })
-        .catch((error) => {
-          console.error("Error fetching chapter data:", error);
+setChapterDataArray(data);
+
         })
         
       }
     }
-  },[selectedChapter,boxDataArray])
+  }, [selectedChapter, boxDataArray]);
+
+  const handleChange = (event) => {
+    setSelectedItem(event.target.value);
+  }
+
+  const handleChapterChange = (event) => {
+    setSelectedChapter(event.target.value);
+  }
 
   return (
-<div className="App">
+    <div className="App">
       <select value={selectedItem} onChange={handleChange} className="languages">
         {LanguagesAvailable.map((item, id) => (
           <option key={id} value={item.name}>
@@ -90,28 +94,28 @@ setBoxDataArray(data.quran)
           </option>
         ))}
       </select>
-      <select value={selectedChapter} onChange={handleChapterChange} className='chapters'>
-        {chapterDataArray.map((item,id)=>(
-<option key={id} value={item.chapter}>
-  {item.chapter}
-</option>
-        ))}
-      </select>
+      {boxDataArray && boxDataArray.length > 0 && (
+        <select value={selectedChapter} onChange={handleChapterChange} className='chapters'>
+          {boxDataArray.map((item, id) => (
+            <option key={id} value={item.chapter}>
+              {item.chapter}
+            </option>
+          ))}
+        </select>
+      )}
       <div className='section'>
         <div className='langdiv'>
           <p>Language: {selectedItem}</p>
         </div>
-      
-          {boxDataArray
+        {chapterDataArray && chapterDataArray.length > 0 && (
+          chapterDataArray
           .map((item, index) => (
-          
             <div key={index}>
-              
               <p>Verse: {item.verse}</p>
               <p>Text: {item.text}</p>
             </div>
-          ))}
-        
+          ))
+        )}
       </div>
     </div>
   );
